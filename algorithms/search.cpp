1a9d7702e909d8becec82ec42d2f628683582219
enerzilius <efblouback@gmail.com>
2026-09-08 16:22:23 -0300

    feat: printTable()

--- algorithms/search.cpp
+++ algorithms/search.cpp
@@ -1,9 +1,9 @@
 #include "search.h"
 #include <cstdint>
-#include <iostream>
 #include <map>
 #include <unordered_set>
 #include <vector>
+#include <iostream>
 
 struct TableContents {
   bool marked;
@@ -12,19 +12,27 @@ struct TableContents {
 
 static std::map<uint16_t, TableContents> markedTable;
 
+static void printTable() {
+  std::cout<<"v | marked | edgeTo \n";
+  for(auto const& [key, value] : markedTable) {
+    std::cout<<key<<" | "<<(value.marked? "T" : "F")<<" | "<<value.edgeTo<<"\n";
+  }
+}
+
 void depthFirstSearch(const std::map<uint16_t, std::vector<uint16_t>> &list,
                       uint16_t begin_at = 0) {
   std::unordered_set<uint16_t> marked;
   uint32_t count = 0;
 
+  markedTable.insert({begin_at, {true, begin_at}});
   depthSearch(list, count, marked, begin_at);
+  printTable();
 }
 
 void depthSearch(const std::map<uint16_t, std::vector<uint16_t>> &list,
                  uint32_t &count, std::unordered_set<uint16_t> &marked,
                  const uint16_t begin_at = 0) {
   marked.insert(begin_at);
-  std::cout << begin_at << " -> ";
   count++;
   
   std::vector<uint16_t> currentVector = list.at(begin_at);
@@ -34,7 +42,7 @@ void depthSearch(const std::map<uint16_t, std::vector<uint16_t>> &list,
     for (const uint16_t vertex : currentVector) {
       if (marked.find(vertex) != marked.end())
       continue;
-    markedTable.insert(vertex, {true, begin_at});
+    markedTable.insert({vertex, {true, begin_at}});
       
     marked.insert(vertex);
     count++;
@@ -43,6 +51,4 @@ void depthSearch(const std::map<uint16_t, std::vector<uint16_t>> &list,
   }
 }
 
-static void printTable() {
 
-}
